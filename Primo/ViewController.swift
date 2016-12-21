28c95aab588d65064e4c561076ee06558f977e93
Macmini <macmini@Macminis-Mac-mini.local>
2016-12-21 17:38:46 +0700

    Initial Commit

---
+++ Primo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Primo
+//
+//  Created by Macmini on 12/21/2559 BE.
+//  Copyright © 2559 Chalee Pin-klay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
